437a5f5a4537d42074d74c5d619d9c08c632d9b4
Carolista <cjones.renaissancewoman@gmail.com>
2026-09-06 04:30:09 -0400

    edited log for successful proxy server run

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.post('/generate-image', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`🚀 Local WW20 Proxy Server running on port ${PORT}`);
+	console.log(`🚀 Proxy Server running on port ${PORT}`);
 });
